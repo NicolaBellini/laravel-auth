4ba8d80750e4ff31905338b67fe44de903671175
NicolaBellini <bellininicola4366@gmail.com>
2024-05-21 15:39:22 +0200

    [add] seeder, helper and seeded DB

---
+++ app/functions/Helper.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\functions;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+
+class Helper{
+     public static function generateSlug(string $string, $model){
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+
+        $exist = $model::where('slug', $slug)->first();
+
+        $c = 1;
+        while($exist){
+            $slug = $original_slug.'-'.$c;
+            $exist = $model::where('slug', $slug)->first();
+            $c++;
+
+        }
+        return $slug;
+
+    }
+}
--- database/migrations/2024_05_21_130741_create_projects_table.php
+++ database/migrations/2024_05_21_130741_create_projects_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name',100);
             $table->string('topic',200);
             $table->tinyInteger('difficulty');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
